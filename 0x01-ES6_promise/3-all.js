848f6e41acd45d4261e3f00eef2d9550cf1092cb
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-02 03:34:14 +0100

    EC6-js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
-import {uploadPhoto, createUser} from './utilis';
-
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((res) => {
-      console.log(`${res[0].body} ${res[1]} ${res[2]}`);
+      console.log(`${res[0].body} ${res[1].firstName} ${res[2].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
