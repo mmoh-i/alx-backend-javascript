f55af844958f283511e4ccd9c7e7914175aa6aa0
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-03 01:50:04 -0800

    EC6-Basics

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
     getNumberOfDepartments(employeesList) {
-      return OBbject.keys(employeesList).length;
+      return Object.keys(employeesList).length;
     },
   };
 }
