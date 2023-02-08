1e600b268713f6e396070bea13db4d4f4b62f4ff
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-08 07:03:37 -0800

    js-classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,8 +2,4 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
-
-  _maxStundentsSize() {
-    console.log(this._maxStudentsSize);
-  }
 }
