506779486187d755d5a5058db9e325ece48d3c0c
mmoh-i <ibrahimmusty100@yahoo.com>
2023-02-28 11:48:41 +0000

    ambient_function

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,28 @@
+/// <refrence path="crud.d.ts" />
+import { RowID, RowElement } from ../interface;
+import * from '../crud';
+
+const row: RowElement {
+  firstName: "Guillaume";
+  lastName: "Salva";
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {
+  ..rows,
+  age: 23;
+};
+
+CRUD.updateRow(newRowID, updtadRow);
+CRUD.deleteRow(newRowID);
+
+const obj = {firstName: "Guillaume", lastName: "Salva"};
+CRUD.insertRow(obj)
+// Insert row {firstName: "Guillaume", lastName: "Salva"}
+
+const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+// Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
+
+CRUD.deleteRow(125);
+// Delete row id 125
