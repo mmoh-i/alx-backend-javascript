480132746f98fbaefc0d4cd89a33b279ef5aacaf
mmoh-i <ibrahimmusty100@yahoo.com>
2023-03-01 17:12:05 +0000

    Set data structure

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,8 @@
-export default setFromArray(arr) {
-  return new Set(arr);
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
 }
