c7132e017fbfb8104062119f7211dfb7467c0d98
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-29 15:35:50 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]
-    ).then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[2].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+  return Promise.all([p1, p2]).then((value) => {
+    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
