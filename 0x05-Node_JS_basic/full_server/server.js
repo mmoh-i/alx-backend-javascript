a097347939ed056bf5568cdc1dfcf68735a75f57
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-05-05 18:10:03 +0100

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+import express from 'express';
+import mapRoutes from './routes';
+
 const app = express();
-const indexRoutes = require('./routes/index');
+const PORT = 1245;
 
-const port = 1245;
-const hostname = '127.0.0.1';
+mapRoutes(app);
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
 
-app.use('/', router);
-app.use('/students', router);
-app.use('/students/:major', router);
-
-app.listen(port, hostname);
-module.export = app;
+export default app;
+module.exports = app;
